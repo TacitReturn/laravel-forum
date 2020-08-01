df6a5e5d1eaf4c1edfdce44ca795bf80b4a8aa52
Glenn Rudge <glennrudge@outlook.com>
2020-08-01 00:57:50 -0400

    Created StorDiscussion Request, and made changes to discussions/create view

--- resources/views/discussions/create.blade.php
+++ resources/views/discussions/create.blade.php
@@ -1,1 +1,33 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">{{ __('Add Discussion') }}</div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('discussions.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="discussion-name">Title</label>
+                    <input name="title" value="" class="form-control" type="text" id="discussion-name">
+                </div>
+                <div class="form-group">
+                    <label for="discussion-content">Content</label>
+                    <textarea class="form-control" name="content" id="discussion-content" cols="30"
+                              rows="10"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="channel">Channel</label>
+                    <select name="channel" id="channel" class="form-control">
+                        @foreach($channels as $channel)
+                            <option value="{{ $channel->id }}"> {{ $channel->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <div class="form-group">
+                        <button class="form-control btn-block btn-success">Submit</button>
+                    </div>
+            </form>
+        </div>
+    </div>
+@endsection
